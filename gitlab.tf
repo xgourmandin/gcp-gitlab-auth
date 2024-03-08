76408e1b49a0d48a8e290ba28d4c4a102ec26305
YoannMet <162726598+YoannMet@users.noreply.github.com>
2024-03-08 16:51:31 +0100

    manage environnement variable + save project_id into CI var

--- gitlab.tf
+++ gitlab.tf
@@ -1,13 +1,20 @@
 resource "gitlab_project_variable" "workload_identity_provider" {
   for_each = toset(var.gitlab_project_ids)
-  key     = "workload_id_provider_id"
+  key     = "workload_id_provider_id_${var.environment}"
   project = each.value
   value   = google_iam_workload_identity_pool_provider.default.name
 }
 
 resource "gitlab_project_variable" "sa_email" {
   for_each = toset(var.gitlab_project_ids)
-  key     = "service_account_email"
+  key     = "service_account_email_${var.environment}"
   project = each.value
   value   = google_service_account.default.email
+}
+
+resource "gitlab_project_variable" "project_id" {
+  for_each = toset(var.gitlab_project_ids)
+  key     = "gcp_project_id_${var.environment}"
+  project = each.value
+  value   = var.gcp_project_id
 }
